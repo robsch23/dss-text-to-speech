c0be1bca16dbbea8126f61fde647a23b0c5e17ab
pyroby <r.schiavelli@ilivetech.it>
2024-02-07 21:13:47 +0100

    fix in recipe.json and recipe.py

--- custom-recipes/text-to-speech-extract/recipe.py
+++ custom-recipes/text-to-speech-extract/recipe.py
@@ -1,5 +1,6 @@
 from dataiku.customrecipe import get_input_names_for_role, get_output_names_for_role, get_recipe_config
 from gtts import gTTS
+import os
 
 # get_recipe_config restituisce un dizionario con le chiave corrispondenti ai params definiti in recipe.json
 
@@ -7,17 +8,17 @@ from gtts import gTTS
 dataset_name = get_input_names_for_role('dataset_input')[0]
 
 # For outputs, the process is the same:
-output_name = get_output_names_for_role('dataset_output')[0] # se è una folder allora mi ritroverò qui il nome della folder
+output_name = get_output_names_for_role('output_folder')[0]
+output_folder = dataiku.Folder(output_name).get_info().get('path')
 
 text_column_name = get_recipe_config().get('text_column_name')
+output_files_type = get_recipe_config().get('output_files_type')
 
 # Read recipe inputs
-df_input = dataiku.Dataset(dataset_name).get_dataframe()
+df = dataiku.Dataset(dataset_name).get_dataframe()
 
-tts = gTTS('text')
-tts.save('path + .mp3')
+list_text = df[text_column_name].to_list()
 
-
-# Write recipe outputs
-output_dataset = dataiku.Dataset(output_name)
-output_dataset.write_with_schema(df)
+for text in list_text:
+    tts = gTTS(text)
+    tts.save(os.path.join(output_folder, f'filename.{str(output_files_type)}'))
